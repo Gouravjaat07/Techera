8a77cad07525e31e6abdf343eec9b9caa1eeb916
Gourav <gouravbarwal001@gmail.com>
2026-02-24 21:50:50 +0530

    Add New Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
-import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './App.css'
-import Home from './pages/Home';
-import Team from './pages/Team';
+import Home from "./pages/Home";
+import Team from "./pages/Team";
+import EventPage from "./pages/Events";
 
 function App() {
-
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/team" element={<Team />} />
-        </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/"     element={<Home />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/events" element={<EventPage />} />
+      </Routes>
     </BrowserRouter>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
